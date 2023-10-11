62ece8934015e2aaf88412ac6b7c9bce7f123713
Abdelrahmen <zzaazzaa1000@gmail.com>
2023-10-11 22:44:42 +0300

    Migrations

--- Foodfella.EF/AppDbContext.cs
+++ Foodfella.EF/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Foodfella.Core.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -8,9 +9,17 @@ using System.Threading.Tasks;
 
 namespace Foodfella.EF
 {
-	public class AppDbContext : IdentityDbContext //add the user
+	public class AppDbContext : IdentityDbContext<ApplicationUser>
 	{
-		AppDbContext(DbContextOptions options) : base(options) { }
+		public AppDbContext(DbContextOptions options) : base(options) { }
+
+		public AppDbContext() { }
+
+		public DbSet<Restaurant> Restaurants { get; set; }
+		public DbSet<MenuItem> MenuItems { get; set; }
+		public DbSet<CartItem> CartItems { get; set; }
+		public DbSet<Order> Orders { get; set; }
+		public DbSet<OrderDetail> OrderDetails { get; set; }
 
 	}
 }
