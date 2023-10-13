bd0f61a524b31dd7eff0c9b05eed3b6b95f307f2
Abdelrahmen <zzaazzaa1000@gmail.com>
2023-10-13 17:36:34 +0300

    base repository interface

---
+++ Foodfella.Core/Interfaces/IBaseRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foodfella.Core.Interfaces
+{
+	public interface IBaseRepository<T> where T : class
+	{
+		// Create
+		void Add(T entity);
+		Task AddAsync(T entity);
+		Task AddRangeAsync(IEnumerable<T> entities);
+
+		// Read
+		T GetById(int id);
+		Task<T> GetByIdAsync(int id);
+		IEnumerable<T> Find(Func<T, bool> predicate);
+		Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate);
+		IEnumerable<T> GetAll();
+		Task<IEnumerable<T>> GetAllAsync();
+
+		// Update
+		void Update(T entity);
+
+		// Delete
+		void Remove(T entity);
+		void RemoveRange(IEnumerable<T> entities);
+	}
+}
