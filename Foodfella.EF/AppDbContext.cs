e7f9c6ad75de96a32c6d98bc0a5348c1320bc12e
Abdelrahmen <zzaazzaa1000@gmail.com>
2023-10-14 22:25:25 +0300

    seeding roles and super admin

--- Foodfella.EF/AppDbContext.cs
+++ Foodfella.EF/AppDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Foodfella.Core.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -21,5 +22,36 @@ namespace Foodfella.EF
 		public DbSet<Order> Orders { get; set; }
 		public DbSet<OrderDetail> OrderDetails { get; set; }
 
+
+		protected override void OnModelCreating(ModelBuilder builder)
+		{
+			base.OnModelCreating(builder);
+			builder.Entity<IdentityRole>().HasData(SeedRoles);
+			builder.Entity<ApplicationUser>().HasData(SeedSuperAdmin());
+		}
+
+		private IdentityRole[] SeedRoles = new IdentityRole[]
+		{
+			new IdentityRole { Id = "1", Name = "SuperAdmin", NormalizedName = "SUPERADMIN" },
+			new IdentityRole { Id = "2", Name = "Admin", NormalizedName = "ADMIN" },
+		};
+
+		private ApplicationUser SeedSuperAdmin()
+		{
+			var hasher = new PasswordHasher<ApplicationUser>();
+			return new ApplicationUser
+			{
+				Id = "1",
+				UserName = "TemporaryUsername",
+				NormalizedUserName = "TEMPORARY-USERNAME",
+				Email = "TemporaryEmail@example.com",
+				NormalizedEmail = "TEMPORARYEMAIL@EXAMPLE.COM",
+				FullName = "Temporary Full Name",
+				address = "Temporary Address",
+				EmailConfirmed = true,
+				PasswordHash = hasher.HashPassword(null, "TemporaryPassword"),
+				SecurityStamp = string.Empty
+			};
+		}
 	}
 }
---
+++ Foodfella.EF/Migrations/20231014192359_seedRolesAndSuperAdmin.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Foodfella.EF.Migrations
+{
+    public partial class seedRolesAndSuperAdmin : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "1", "f5df5677-42f7-4d1a-a9a3-735f9e59be7c", "SuperAdmin", "SUPERADMIN" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "2", "16a198f3-61d7-4464-ad74-0bbfbb9b52c8", "Admin", "ADMIN" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUsers",
+                columns: new[] { "Id", "AccessFailedCount", "ConcurrencyStamp", "Email", "EmailConfirmed", "FullName", "LockoutEnabled", "LockoutEnd", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "TwoFactorEnabled", "UserName", "address" },
+                values: new object[] { "1", 0, "ecd2646b-bdc3-4e58-906d-7e8ba3666c31", "TemporaryEmail@example.com", true, "Temporary Full Name", false, null, "TEMPORARYEMAIL@EXAMPLE.COM", "TEMPORARY-USERNAME", "AQAAAAEAACcQAAAAEOfGEOaNGqmV0tAhCLNpJxWet3t081FgrB+mo5crLioFtlw+JnLWc2sUktS+UugYag==", null, false, "", false, "TemporaryUsername", "Temporary Address" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "1");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "2");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "1");
+        }
+    }
+}
