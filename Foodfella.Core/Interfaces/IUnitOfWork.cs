5d1ab4460b130282005e81bfcb311b58b160b8e9
Abdelrahmen <zzaazzaa1000@gmail.com>
2023-10-14 22:01:59 +0300

    unit of work interface

---
+++ Foodfella.Core/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,20 @@
+﻿using Foodfella.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foodfella.Core.Interfaces
+{
+	public interface IUnitOfWork: IDisposable
+	{
+		IBaseRepository<Restaurant> Restaurants { get; }
+		IBaseRepository<MenuItem> MenuItems { get; }
+		IBaseRepository<CartItem> CartItems { get; }
+		IBaseRepository<Order> Orders { get; }
+		IBaseRepository<OrderDetail> OrderDetails { get; }
+
+		int Complete();
+	}
+}
