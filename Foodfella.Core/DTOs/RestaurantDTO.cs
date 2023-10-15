3241747c94b945eac7e1e438ce1ddd849bcd2b29
Abdelrahmen <zzaazzaa1000@gmail.com>
2023-10-15 23:22:25 +0300

    restaurant DTOSs

---
+++ Foodfella.Core/DTOs/RestaurantDTO.cs
@@ -0,0 +1,27 @@
+﻿using Foodfella.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foodfella.Core.DTOs
+{
+	public class RestaurantDTO
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string CuisineType { get; set; }
+		public string Location { get; set; }
+		public double AverageRating { get; set; }
+
+		public static RestaurantDTO FromRestaurant(Restaurant restaurant) => new RestaurantDTO
+		{
+			Id = restaurant.Id,
+			Name = restaurant.Name,
+			CuisineType = restaurant.CuisineType,
+			Location = restaurant.Location,
+			AverageRating = restaurant.AverageRating
+		};
+	}
+}
---
+++ Foodfella.Core/DTOs/RestaurantUpdateDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foodfella.Core.DTOs
+{
+	public class RestaurantUpdateDTO
+	{
+		public string Name { get; set; }
+		public string CuisineType { get; set; }
+		public string Location { get; set; }
+	}
+}
