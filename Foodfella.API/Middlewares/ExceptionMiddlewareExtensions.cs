727c6c77503662132f34289602792f9d739a4afe
Abdelrahmen <zzaazzaa1000@gmail.com>
2023-10-15 21:39:05 +0300

    Global Exception Handling

---
+++ Foodfella.API/Middlewares/ExceptionMiddlewareExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Foodfella.Core.Models;
+using System.Text.Json;
+
+namespace Foodfella.API.Extentions
+{
+	public class GlobalExceptionHandlingMiddleware : IMiddleware
+	{
+		public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+		{
+			try
+			{
+				await next(context);
+			}
+			catch (Exception ex)
+			{
+				context.Response.StatusCode = 500;
+				var errorDetails = new ErrorDetails
+				{
+					StatusCode = 500,
+					Message = ex.Message,
+				};
+
+				var json = JsonSerializer.Serialize(errorDetails);
+				
+				await context.Response.WriteAsync(json);
+
+				context.Response.ContentType = "application/json";
+			}
+		}
+	}
+}
