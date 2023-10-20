4cd803e9c2e262a37619e2f1653d7f3804b0cb08
Abdelrahmen <zzaazzaa1000@gmail.com>
2023-10-20 22:26:58 +0300

    Cart Items Dtos

---
+++ Foodfella.Core/DTOs/CartItemCreateDTO.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Foodfella.Core.DTOs
+{
+	public class CartItemCreateDTO
+	{
+		[Required]
+		public int MenuItemId { get; set; }
+
+		[Required]
+		[Range(1, 100)]
+		public int Quantity { get; set; }
+
+		[Required]
+		[Range(0, double.MaxValue)]
+		public decimal Price { get; set; }
+	}
+
+}
---
+++ Foodfella.Core/DTOs/CartItemDTO.cs
@@ -0,0 +1,31 @@
+﻿using Foodfella.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foodfella.Core.DTOs
+{
+	public class CartItemDTO
+	{
+		public int Id { get; set; }
+		public int MenuItemId { get; set; }
+		public int Quantity { get; set; }
+		public decimal Price { get; set; }
+		public DateTime CreatedAt { get; set; }
+		public DateTime UpdatedAt { get; set; }
+
+		public static object FromCartItem(CartItem c)
+		=> new CartItemDTO
+		{
+			CreatedAt = c.CreatedAt,
+			Id = c.Id,
+			MenuItemId = c.MenuItemId,
+			Quantity = c.Quantity,
+			Price = c.Price,
+			UpdatedAt = c.UpdatedAt,
+		};
+	}
+
+}
