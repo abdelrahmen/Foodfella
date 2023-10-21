38cd65941ebe3859210c1fa2010a53696570c852
Abdelrahmen <zzaazzaa1000@gmail.com>
2023-10-21 23:30:11 +0300

    Order and Order Details DTOs

---
+++ Foodfella.Core/DTOs/OrderDTO.cs
@@ -0,0 +1,39 @@
+﻿using Foodfella.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foodfella.Core.DTOs
+{
+	public class OrderDTO
+	{
+		public int Id { get; set; }
+
+		[Required]
+		public string UserId { get; set; }
+
+		[Required]
+		public DateTime OrderDate { get; set; }
+
+		[Required]
+		[MaxLength(50)]
+		public string PaymentMethod { get; set; }
+
+		[Required]
+		[MaxLength(200)]
+		public string PickupAddress { get; set; }
+
+		public static OrderDTO FromOrder(Order order) => new OrderDTO
+		{
+			Id = order.Id,
+			UserId = order.UserId,
+			OrderDate = order.OrderDate,
+			PaymentMethod = order.PaymentMethod,
+			PickupAddress = order.PickupAddress,
+		};
+	}
+}
---
+++ Foodfella.Core/DTOs/OrderDetailsDTO.cs
@@ -0,0 +1,39 @@
+﻿using Foodfella.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foodfella.Core.DTOs
+{
+	public class OrderDetailsDTO
+	{
+		public int Id { get; set; }
+
+		[Required]
+		public int OrderId { get; set; }
+
+		[Required]
+		public int MenuItemId { get; set; }
+
+		[Required]
+		[Range(1, 100)]
+		public int Quantity { get; set; }
+
+		[Required]
+		[Range(0, double.MaxValue)]
+		public decimal Price { get; set; } //price at the moment of making order
+
+		public static OrderDetail FromOrderDetail(OrderDetail orderDetail) => new OrderDetail
+		{
+			Id = orderDetail.Id,
+			OrderId = orderDetail.OrderId,
+			MenuItemId = orderDetail.MenuItemId,
+			Quantity = orderDetail.Quantity,
+			Price = orderDetail.Price
+		};
+	}
+}
