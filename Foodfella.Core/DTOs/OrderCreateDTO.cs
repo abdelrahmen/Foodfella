a21c8f2f1bec029f32ae344312829c87bfe6ff2b
Abdelrahmen <zzaazzaa1000@gmail.com>
2023-10-22 22:34:28 +0300

    create order DTO

---
+++ Foodfella.Core/DTOs/OrderCreateDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Foodfella.Core.DTOs
+{
+	public class OrderCreateDTO
+	{
+		[Required]
+		public List<CartItemCreateDTO> CartItems { get; set; }
+		[Required]
+		[MaxLength(200)]
+		public string PickupAddress { get; set; }
+		[Required]
+		[MaxLength(50)]
+		public string PaymentMethod { get; set; }
+	}
+}
