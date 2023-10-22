fb45c0483213d16bdf261626f3c0e238680ff68a
Abdelrahmen <zzaazzaa1000@gmail.com>
2023-10-22 23:24:13 +0300

    order update dto

---
+++ Foodfella.Core/DTOs/OrderUpdateDTO.cs
@@ -0,0 +1,15 @@
+﻿using Foodfella.Core.DTOs;
+using Foodfella.Core.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace Foodfella.API.Controllers
+{
+	public class OrderUpdateDTO
+	{
+		public int Id { get; set; }
+
+		[Required]
+		[MaxLength(15)]
+		public string Status { get; set; }
+	}
+}
